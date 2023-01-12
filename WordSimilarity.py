3293bf6386ace4df3363f34c2c5067241f409702
NoFac3 <49742353+NoFac3@users.noreply.github.com>
2023-01-12 15:11:23 -0600

    Update WordSimilarity.py

--- WordSimilarity.py
+++ WordSimilarity.py
@@ -6,17 +6,6 @@ def Tokenize(word):
     token = nlp(word)
     return token
 
-def token_vector(word):
-    token = Tokenize(word)
-    if token.has_vector==True:
-        return token.vector
-    else:
-        return 0
-
-def token_vector_norm(word):
-    token = Tokenize(word)
-    return token.vector_norm
-
 def Similarity(words):
     token1, token2 = Tokenize(words)
     return token1.similarity(token2)
@@ -29,24 +18,18 @@ def SimilarLoop(words):
             S.append(Similarity(s))
     return S
 
+def WordVector(word):
+    token = Tokenize(word)
+    if token.has_vector==True:
+        return token.vector
+    else:
+        return 0
+
 def WordNorm(word):
-    token = nlp(word)
-    norm = token.vector_norm
-    return norm
+    token = Tokenize(word)
+    return token.vector_norm
 
 def RootWord(word):
     lem = WNL()
     w = lem.lemmatize("".join(word))
     return w
-
-from numpy import log as ln
-
-def entropy(dist):
-    su = 0
-    for p in dist:
-        r = p/sum(dist)
-        if r==0:
-            su+=0
-        else:
-            su+= -r*(ln(r))
-    return su/ln(2)
